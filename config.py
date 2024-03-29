3e2e0d0be90fd1e98ec169b1f87a606b5f879a5d
rumiantsevaa <89034072+rumiantsevaa@users.noreply.github.com>
2024-03-29 13:55:39 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 TELEGRAM_TOKEN = "SECRET"
 
+# URL для отправки изображения
+url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendPhoto'
+
+sqlite_file = "SECRET"
+
+okay_sticker = "SECRET"
+game_over_sticker = "SECRET"
+cool_dogs_sticker = "SECRET"
+winner_sticker = "SECRET"
